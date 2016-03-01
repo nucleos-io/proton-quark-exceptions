fa9045b4395557d94d889764ab9f53e79ef5ff70
luish000 <luish000.lh@gmail.com>
2016-03-01 16:00:48 -0430

    Updated version
     - Renamed methods _bindToProton and _bindToApp of bindToProton and bindToApp
     - Expose exceptions and bindToApp inside initialize method

--- index.js
+++ index.js
@@ -4,46 +4,78 @@
 const Quark = require('proton-quark')
 const path = require('path')
 const _ = require('lodash')
+const stackTrace = require('stack-trace')
 
-module.exports = class ExceptionsQuark extends Quark {
+/**
+ * @class ExceptionsQuark
+ * @classdesc This quark is for expose as global the proton exceptions
+ * @author Luis Hernandez
+ */
+class ExceptionsQuark extends Quark {
 
   constructor(proton) {
     super(proton)
   }
 
+  /**
+   * @override
+   * @method configure
+   * @description Ask if the proton.app.exceptions object exist, if not exist
+   * the method create the proton.app.exceptions object
+   * @author Luis Hernandez
+   */
   configure() {
     if (!this.proton.app.exceptions)
       this.proton.app.exceptions = {}
     return true
   }
 
+  /**
+   * @override
+   * @method initialize
+   * @description bind to the app object every exceptions and bind to proton an
+   * error manager
+   * @author Luis Hernandez
+   */
   initialize() {
-    this._bindToApp()
-    this._bindToProton()
+    _.forEach(this._exceptions, exception => {
+      exception.expose(exception)
+      this.bindToApp(exception)
+    })
   }
 
-  _bindToApp() {
+  /**
+   * @override
+   * @method bindToApp
+   * @description Add to the app exception object the exceptions
+   * @author Luis Hernandez
+   */
+  bindToApp(...args) {
+    const exception = args[0]
+    this.proton.app.exceptions[exception.name] = exception
+  }
+
+  /**
+   * @override
+   * @method bindToProton
+   * @description Add an error manager as middleware in the app
+   * @author Luis Hernandez
+   */
+  bindToProton() {
+    // TODO
+  }
+
+  get _exceptions() {
     const exceptionsPath = path.join(this.proton.app.path, '/exceptions')
-    const exceptions = require('require-all')(exceptionsPath)
-    _.forEach(exceptions, Exception => {
-      global[Exception.prototype.constructor.name] = Exception
-    })
+    return require('require-all')(exceptionsPath)
   }
 
-  _bindToProton() {
-    this.proton.use(function*(next) {
-      try {
-        yield next
-      } catch (err) {
-        if (err.handle) {
-          err.handle(this)
-        } else {
-          this.status = 500
-          this.body = err.message
-        }
-        this.app.emit('error', err, this)
-      }
-    })
+  * exceptionManager(next) {
+    try {
+      yield next
+    } catch (err) {
+      const trace = stackTrace.parse(err)
+    }
   }
 
 }
